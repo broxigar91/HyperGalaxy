e6bdf7d295d5a651a4a9e44500ac87135523171a
broxigar91 <ichigobiar@gmail.com>
2017-10-19 10:53:31 +0200

    Prototipo inicial

---
+++ HyperGalaxy/Assets/Scripts/EnemyContainer.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyContainer : MonoBehaviour {
+
+    float leftlimit;
+    float rightlimit;
+
+    bool orientation; //izquierda (falso), derecha (verdadero)
+
+	
+    // Use this for initialization
+	void Start ()
+    {
+        leftlimit = -1.5f;
+        rightlimit = 1.5f;
+        orientation = false;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {	
+        if(orientation)//derecha
+        {
+            if(gameObject.transform.position.x < rightlimit)
+                gameObject.transform.Translate(Vector3.right* Time.deltaTime);
+            else
+                orientation = false;
+        }
+        else//izquierda
+        {
+            if (gameObject.transform.position.x > leftlimit)
+                gameObject.transform.Translate(Vector3.left*Time.deltaTime);
+            else
+                orientation = true;
+        }
+
+
+        if(transform.childCount==0)
+        {
+            Debug.Log("WIN");
+        }
+	}
+}
---
+++ HyperGalaxy/Assets/Scripts/GameManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    private int score;
+
+    public static GameManager instance;
+
+    private void Awake()
+    {
+        if(instance==null)
+        {
+            instance = this;
+        }
+        else if(instance!=this)
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Use this for initialization
+    void Start () {
+        score = 0;
+	}
+	
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public int Score
+    {
+        get
+        {
+            return score;
+        }
+
+        set
+        {
+            score = value;
+        }
+    }
+
+}
