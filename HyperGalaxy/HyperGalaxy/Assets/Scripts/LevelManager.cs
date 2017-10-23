ef58326a7f04d624bb951888c192f7e1c6accbdf
broxigar91 <ichigobiar@gmail.com>
2017-10-23 11:32:27 +0200

    Se mueve y dispara

---
+++ HyperGalaxy/HyperGalaxy/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+    public static LevelManager instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void LoadLevel(string scene)
+    {
+        SceneManager.LoadScene("");
+    }
+}
